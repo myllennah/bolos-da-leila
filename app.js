764b4bbdf205fa0d9b3728b7b0da680570ed87b5
Yuri Lima <145035100+YuriLima66@users.noreply.github.com>
2024-10-16 11:23:51 -0300

    ajustando links

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require ('express');
+const express = require('express');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
 //acessando a rota
 app.get('/', (req, res) => {
+
 //passando parâmetro head
 res.render('index');
 });
 
 //nova rota
-app.get('/sobre', (req, res) => {
+app.get('/historia', (req, res) => {
     res.render('historia');
+});
+
+app.use('/sobre', (req, res) => {
+
+res.redirect('/historia');
 });
 
 //redirecionamento de página
@@ -21,17 +26,16 @@ app.get('/doces', (req, res) => {
     res.render('doces');
 });
 
-app.get('/cardapio', (req, res) =>{
+app.get('/cardapio', (req, res) => {
     res.render('cardapio');
-    });
-
-app.get('/contato', (req, res) =>{
-res.render('contato');
 });
 
+app.get('/contato', (req, res) => {
+    res.render('contato');
+});
 
-app.use((req, res) =>{
-res.status(404).render('404');
+app.use((req, res) => {
+    res.status(404).render('404');
 });
 
 app.listen(3007);
