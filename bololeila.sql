95d5dee2d920e7deea2c625aa928bd99007e883b
myllennah <milena.rmota@senacsp.edu.br>
2024-10-28 12:02:19 -0300

    atualização banco de dados

--- bololeila.sql
+++ bololeila.sql
@@ -1,6 +1,5 @@
 CREATE database bolosdaleila
 
-
 CREATE TABLE `cadastro` (
   `nome_cliente` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -8,7 +7,22 @@ CREATE TABLE `cadastro` (
   `senha` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE produto (
+    id_produto INT(4) primary key AUTO_INCREMENT,
+    nome_produto VARCHAR(20) NOT NULL UNIQUE,
+    descricao VARCHAR(1000) NOT NULL,
+    categoria VARCHAR(12) NOT NULL,
+    preco FLOAT(5,2) NOT NULL
+);
 
+CREATE TABLE pedido (
+    id_pedido INT(12) primary key AUTO_INCREMENT,
+    itens_pedido VARCHAR(1000) NOT NULL,
+    data_pedido DATE NOT NULL,
+    valor_total FLOAT(5,2) NOT NULL,
+    pagamento VARCHAR(12) NOT NULL,
+    status_pedido VARCHAR(12) NOT NULL
+);
 
 INSERT INTO `cadastro` ( `nome_cliente`, `email`, `telefone`, `senha`) VALUES
 ( 'Julia Melo Alves', 'julinhamelo@dayrep.com', '11960452493', 'LaeCh7sap'),
--- conexaobanco.js
+++ conexaobanco.js
@@ -4,8 +4,8 @@ var mysql = require("mysql");
 
 const conexaoBanco = mysql.createConnection({
     host: 'localhost',
-    user: 'yuri',
-    password: '', // Se você tiver uma senha, coloque-a aqui
+    user: 'admin',
+    password: '000000', // Se você tiver uma senha, coloque-a aqui
     database: 'cardapio_leilabolos'
 });
 
