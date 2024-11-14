6e4cb5f61071f7ed02797c755e3def925f386a4c
Yuri Lima <145035100+YuriLima66@users.noreply.github.com>
2024-11-14 09:52:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
 
 // Configuração do Express
 app.use(express.static('public'));
+const path= require('path');
+app.use(express.static((__dirname,'public')));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true }));
