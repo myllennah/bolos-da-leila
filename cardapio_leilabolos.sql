821a844f20062b5575511101646fefc2a3228891
myllennah <milena.rmota@senacsp.edu.br>
2024-12-11 00:33:23 -0300

    descrições cardápio

--- cardapio_leilabolos.sql
+++ cardapio_leilabolos.sql
@@ -1,8 +1,6 @@
 -- Cria o banco de dados (se ele não existir)
 CREATE DATABASE cardapio_leilabolos;
 
-
-
 CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -31,14 +29,16 @@ CREATE TABLE pedido (
 
 INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `imagem`, `categoria`) VALUES
 (1, 'Bolo com tema de futebol', 'Tenha um bolo com o seu time do coração!', 50.00, '../img/time1.jpeg', 'bolos'),
-(2, 'Bolo com decoração simples', 'Elegância e sabor se encontram em nossos bolos clássicos com frutas e flores. Decorados com flores delicadas e frutas frescas, esses bolos são perfeitos para qualquer ocasião especial. Escolha o tamanho, o sabor da massa e o recheio, e encante seus convidados com a beleza e o frescor dessa combinação clássica!', 50.00, '../img/chocolate.jpeg', 'bolos'),
-(3, 'Bolo Infantil', 'Transforme a festa em um mundo de fantasia! Com decorações encantadoras, cores pastéis e detalhes mágicos, esses bolos são feitos para encantar e surpreender. Personalize o tamanho, o sabor da massa e o recheio, e adicione um toque de magia ao seu evento!', 50.00, '../img/crianca1.jpeg', 'bolos'),
+(2, 'Bolo com decoração clássica', 'Elegância e sabor se encontram em nossos bolos clássicos com frutas e flores. Decorados com flores delicadas e frutas frescas, esses bolos são perfeitos para qualquer ocasião especial. Escolha o tamanho, o sabor da massa e o recheio, e encante seus convidados com a beleza e o frescor dessa combinação clássica!', 50.00, '../img/chocolate.jpeg', 'bolos'),
+(3, 'Bolo com tema de fantasia', 'Transforme a festa em um mundo de fantasia! Com decorações encantadoras, cores pastéis e detalhes mágicos, esses bolos são feitos para encantar e surpreender. Personalize o tamanho, o sabor da massa e o recheio, e adicione um toque de magia ao seu evento!', 50.00, '../img/crianca1.jpeg', 'bolos'),
 (4, 'Brigadeiro', 'O clássico brigadeiro, feito com chocolate em pó, leite condensado e manteiga. Irresistível!', 90.00, '../img/brigtrad.jpg', 'doces'),
-(5, 'Coxinha', 'Coxinha de frango com massa crocante e recheio saboroso. Um clássico brasileiro irresistível!', 5.00, '../img/coxinha.png', 'salgados');
+(5, 'Coxinha', 'Coxinha de frango com massa crocante e recheio saboroso. Um clássico brasileiro irresistível!', 80.00, '../img/coxinha.png', 'salgados'),
 (6, 'Beijinho', 'Vendido em centos. Delicioso doce de leite condensado, coco e açúcar, coberto com coco ralado. Um clássico brasileiro irresistível!', 90.00, '../img/beijinho.jpg', 'doces');
-(7, 'Bicho de pé', 'Vendido em centos. DDoce saboroso de morango, leite condensado e granulado colorido.', 90.00, '../img/bichodepe.png', 'doces');
-(8, 'Bolo com personagens', 'Dê vida à magia do cinema em sua festa com nossos bolos de personagens de filmes! Com decorações inspiradas em seus personagens e cenas favoritas, esses bolos são perfeitos para fãs de todas as idades. Escolha o tamanho, o sabor da massa e o recheio, e deixe sua celebração cinematográfica ainda mais especial!', 50.00, '../img/personagens.jpeg', 'bolos');
+(7, 'Bicho de pé', 'Vendido em centos. Doce saboroso de morango, leite condensado e granulado colorido.', 90.00, '../img/bichodepe.png', 'doces'),
+(8, 'Bolo com tema de filmes', 'Dê vida à magia do cinema em sua festa com nossos bolos de personagens de filmes! Com decorações inspiradas em seus personagens e cenas favoritas, esses bolos são perfeitos para fãs de todas as idades. Escolha o tamanho, o sabor da massa e o recheio, e deixe sua celebração cinematográfica ainda mais especial!', 50.00, '../img/personagens.jpeg', 'bolos'),
 (9, 'Bolo com super-heróis', 'Deixe a sua festa cheia de ação com nossos bolos de super-heróis! Decorados com os personagens favoritos, símbolos e cores vibrantes, esses bolos são perfeitos para trazer toda a emoção das aventuras dos super-heróis. Escolha o tamanho, o sabor da massa e o recheio, e tenha um bolo que será o herói da sua celebração!', 50.00, '../img/herois.jpeg', 'bolos');
+(10, 'Bolinho de queijo', ' Crocante por fora e recheado com queijo derretido, uma delícia!', 50.00, '../img/herois.jpeg', 'bolos');
+
 
 
 INSERT INTO `cadastro` ( `nome_cliente`, `email`, `telefone`, `senha`) VALUES
